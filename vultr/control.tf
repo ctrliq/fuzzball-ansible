eb6c113421c59b3193bad16463c6b179a493d421
Arian Cabrera <acabrera@ciq.com>
2024-08-01 10:24:08 -0400

    added vpc2

--- vultr/control.tf
+++ vultr/control.tf
@@ -13,6 +13,6 @@ resource "vultr_instance" "ctl" {
   region            = "ewr"
   os_id             = 448 # rocky linux
   hostname          = "${var.prefix}-ctl${count.index+1}"
-  vpc_ids           = [local.cluster_vpc_id]
+  vpc_ids           = [local.cluster_vpc2_id]
   ssh_key_ids       = [vultr_ssh_key.root_ssh_key.id]
 }

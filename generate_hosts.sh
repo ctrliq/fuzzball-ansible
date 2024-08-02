3dc6f195cae22eb4318b03d7f26bd2e75b9665a7
Stephen Simpson <ssimpson@ciq.com>
2024-08-02 15:31:33 -0500

    Added generation script

--- generate_hosts.sh
+++ generate_hosts.sh
@@ -1,55 +1,105 @@
 #! /bin/bash
 
+TF_OUTPUT=$(terraform -chdir=vultr output --json)
+substrate_nfs_subnet=$(echo "$TF_OUTPUT" | jq -r '.substrate_nfs_subnet.value')
+substrate_nfs_mount=$(echo "$TF_OUTPUT" | jq -r '.admin_nodes_private_ip.value.private_IP_admin1')
+metallb_lb_ip=$(echo "$TF_OUTPUT" | jq -r '.controller_node_public_ips.value.public_IP_ctl1')
+keycloak_ip=$(echo "$TF_OUTPUT" | jq -r '.admin_nodes_public_ip.value.public_IP_admin1')
+lb_ip_dashed=${metallb_lb_ip//\./-}
+fz_domain="${lb_ip_dashed}.nip.io"
+keycloak_ip_dashed=${keycloak_ip//\./-}
+keycloak_domain="${keycloak_ip_dashed}.nip.io"
+
+keycloak_uuid=$(uuidgen | tr '[:upper:]' '[:lower:]')
+
+#read -rp "Enter your variable value: " MTN_KEY
+MTN_KEY="foo"
+fuzzball_operator_version="v0.0.1-gcf137a20"
+
 cat << EOF > hosts.yaml
 all:
   vars:
     ansible_user: root
-    mtn_access_key:
-    substrate_nfs_subnet: 10.0.0.0/24
-    substrate_nfs_mount: admin1:/fuzzball/shared
+    mtn_access_key: ${MTN_KEY}
+    substrate_nfs_subnet: ${substrate_nfs_subnet}
+    substrate_nfs_mount: ${substrate_nfs_mount}:/svr/fuzzball/shared
+    FB_POLL_CONFIG: /fuzzball/shared/substrate/substrate-config.yaml
+    fuzzball_operator_version: ${fuzzball_operator_version}
+    fuzzball_operator_storage_class: "local-path"
+    fuzzball_operator_chart: "oci://repository.ciq.com/fuzzball-testing/images/helm/fuzzball-operator"
+    fuzzball_operator_image: "repository.ciq.com/fuzzball-testing/images/fuzzball-operator"
     fuzzball_orchestrate:
       spec:
         fuzzball:
           substrate:
             nfs:
-              server: # IP address of NFS server hosting Fuzzball Substrate config
+              server: ${substrate_nfs_mount}
           kube:
             backendGatewayService:
-              metallb.universe.tf/loadBalancerIPs: # preferred external IP address
+              metallb.universe.tf/loadBalancerIPs: ${metallb_lb_ip}
           workflow:
             callbackService:
               annotations:
-                metallb.universe.tf/loadBalancerIPs: # preferred external IP address
+                metallb.universe.tf/loadBalancerIPs: ${metallb_lb_ip}
         ingress:
           create:
-            domain: # e.g., 10.0.0.99.nip.io
+            domain: ${fz_domain}
             proxy:
               annotations:
-                metallb.universe.tf/loadBalancerIPs: # preferred external IP address
+                metallb.universe.tf/loadBalancerIPs: ${metallb_lb_ip}
         keycloak:
           create:
-            realmId: # Unique UUIDv4, or existing UUIDv4 for external Keycloak
-            ingres:
-              hostname: # e.g., auth.10.0.0.99.nip.io
+            realmId: ${keycloak_uuid}
+            ingress:
+              hostname: auth.${keycloak_domain}
         database:
           create:
             credentials:
-              password: # database password to use
+              password: password
     metallb_pool:
       spec:
         addresses:
-        - # addresses available for use with external services; e.g., "10.0.0.99-10.0.0.100"
+        - ${metallb_lb_ip}/32
+    keycloak_env:
+      KC_DB_PASSWORD: password
+      KEYCLOAK_ADMIN_PASSWORD: password
+      KC_HOSTNAME_URL: https//auth.${keycloak_domain}
+      KC_HOSTNAME_ADMIN_URL: https//authadmin.${keycloak_domain}
+      keycloak_certbot_email: noreply@ciq.co
+      keycloak_certbot_domain: "https//auth.${keycloak_domain},https//authadmin.${keycloak_domain}"
+EOF
+
+cat << EOF >> hosts.yaml
   children:
     admin:
       hosts:
-        admin1:
-          cluster_interface:
+EOF
+echo "$TF_OUTPUT" | jq -r '.admin_nodes_public_ip.value | to_entries[] | "\(.key)=\(.value)"' | while IFS="=" read -r key value; do
+  cat << EOF >> hosts.yaml
+        ${key##*_}:
+          ansible_host: $value
+EOF
+done
+
+cat << EOF >> hosts.yaml
     controller:
       hosts:
-        ctl1:
-          rke2_node_ip:
+EOF
+echo "$TF_OUTPUT" | jq -r '.controller_node_public_ips.value | to_entries[] | "\(.key)=\(.value)"' | while IFS="=" read -r key value; do
+  cat << EOF >> hosts.yaml
+        ${key##*_}:
+          ansible_host: $value
+          #rke2_node_ip:
+EOF
+done
+cat << EOF >> hosts.yaml
     compute:
       hosts:
-        compute[1:4]:
-          cluster_interface:
 EOF
+echo "$TF_OUTPUT" | jq -r '.compute_instances_public_ip.value | to_entries[] | "\(.key)=\(.value)"' | while IFS="=" read -r key value; do
+  cat << EOF >> hosts.yaml
+        ${key##*_}:
+          ansible_host: $value
+          #cluster_interface:
+EOF
+done

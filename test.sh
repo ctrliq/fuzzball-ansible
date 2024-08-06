4e2fb0729f25b48d217b3f77d179cf846603b281
Arian Cabrera <acabrera@ciq.com>
2024-08-06 16:56:02 -0400

    variable export

--- test.sh
+++ test.sh
@@ -1,49 +1,40 @@
-# List of Vultr regions 
-declare -A regions=(
-    ["Amsterdam"]="ams"
-    ["Atlanta"]="atl"
-    ["Bangalore"]="blr"
-    ["Mumbai"]="bom"
-    ["Paris"]="cdg"
-    ["Delhi NCR"]="del"
-    ["Dallas"]="dfw"
-    ["New Jersey"]="ewr"
-    ["Frankfurt"]="fra"
-    ["Honolulu"]="hnl"
-    ["Seoul"]="icn"
-    ["Osaka"]="itm"
-    ["Johannesburg"]="jnb"
-    ["Los Angeles"]="lax"
-    ["London"]="lhr"
-    ["Madrid"]="mad"
-    ["Manchester"]="man"
-    ["Melbourne"]="mel"
-    ["Mexico City"]="mex"
-    ["Miami"]="mia"
-    ["Tokyo"]="nrt"
-    ["Chicago"]="ord"
-    ["São Paulo"]="sao"
-    ["Santiago"]="scl"
-    ["Seattle"]="sea"
-    ["Singapore"]="sgp"
-    ["Silicon Valley"]="sjc"
-    ["Stockholm"]="sto"
-    ["Sydney"]="syd"
-    ["Tel Aviv"]="tlv"
-    ["Warsaw"]="waw"
-    ["Toronto"]="yto"
-)
+# Print the available regions
+echo "
+    [\"Amsterdam\"]=\"ams\"
+    [\"Atlanta\"]=\"atl\"
+    [\"Bangalore\"]=\"blr\"
+    [\"Mumbai\"]=\"bom\"
+    [\"Paris\"]=\"cdg\"
+    [\"New Jersey\"]=\"ewr\"
+    [\"Frankfurt\"]=\"fra\"
+    [\"Honolulu\"]=\"hnl\"
+    [\"London\"]=\"lhr\"
+    [\"Madrid\"]=\"mad\"
+    [\"Manchester\"]=\"man\"
+    [\"Melbourne\"]=\"mel\"
+    [\"Mexico City\"]=\"mex\"
+    [\"Miami\"]=\"mia\"
+    [\"Tokyo\"]=\"nrt\"
+    [\"Chicago\"]=\"ord\"
+    [\"São Paulo\"]=\"sao\"
+    [\"Santiago\"]=\"scl\"
+    [\"Seattle\"]=\"sea\"
+    [\"Singapore\"]=\"sgp\"
+    [\"Silicon Valley\"]=\"sjc\"
+    [\"Stockholm\"]=\"sto\"
+    [\"Sydney\"]=\"syd\"
+    [\"Tel Aviv\"]=\"tlv\"
+    [\"Warsaw\"]=\"waw\"
+    [\"Toronto\"]=\"yto\"
+"
 
-echo "Please select a Vultr region:"
-
-# Display the selection menu
-select abbreviation in "${!regions[@]}"; do
-    # Check if the selection is valid
-    if [[ -n "$abbreviation" ]]; then
-        selected_region=${regions[$abbreviation]}
-        echo "You selected: $selected_region ($abbreviation)"
-        break
-    else
-        echo "Invalid selection. Please try again."
-    fi
+# Prompt the user to input a region
+while true; do
+  read -p "Please type the Vultr region: " region
+  if [[ ${#region} -eq 3 ]]; then
+      echo "You have picked the $region region."
+      break
+  else
+      echo "Region must be a 3-letter string. Please try again."
+  fi
 done

23d7367eb360493debb7779368f17c8a79824dd1
Stephen Simpson <ssimpson@ciq.com>
2024-08-09 10:10:22 -0500

    Fixed YAML formatting

--- terraform_apply.sh
+++ terraform_apply.sh
@@ -308,82 +308,82 @@ function generate_hosts() {
 
 
     cat << EOF > hosts.yaml
-    all:
-    vars:
-        ansible_user: root
-        mtn_access_key: ${MTN_KEY}
-        substrate_nfs_subnet: ${substrate_nfs_subnet}
-        substrate_nfs_mount: ${substrate_nfs_mount}:/srv/fuzzball/shared
-        FB_POLL_CONFIG: /fuzzball/shared/substrate/substrate-config.yaml
-        fuzzball_operator_version: ${fuzzball_operator_version}
-        fuzzball_operator_storage_class: "local-path"
-        fuzzball_operator_chart: "oci://repository.ciq.com/fuzzball-testing/images/helm/fuzzball-operator"
-        fuzzball_operator_image: "repository.ciq.com/fuzzball-testing/images/fuzzball-operator"
-        fuzzball_orchestrate:
-        spec:
-            fuzzball: 
-            substrate:
-                nfs:
-                server: ${substrate_nfs_mount}
-            kube:
-                backendGatewayService:
+all:
+  vars:
+    ansible_user: root
+    mtn_access_key: ${MTN_KEY}
+    substrate_nfs_subnet: ${substrate_nfs_subnet}
+    substrate_nfs_mount: ${substrate_nfs_mount}:/srv/fuzzball/shared
+    FB_POLL_CONFIG: /fuzzball/shared/substrate/substrate-config.yaml
+    fuzzball_operator_version: ${fuzzball_operator_version}
+    fuzzball_operator_storage_class: "local-path"
+    fuzzball_operator_chart: "oci://repository.ciq.com/fuzzball-testing/images/helm/fuzzball-operator"
+    fuzzball_operator_image: "repository.ciq.com/fuzzball-testing/images/fuzzball-operator"
+    fuzzball_orchestrate:
+      spec:
+        fuzzball: 
+          substrate:
+            nfs:
+              server: ${substrate_nfs_mount}
+          kube:
+            backendGatewayService:
+              metallb.universe.tf/loadBalancerIPs: ${metallb_lb_ip}
+          workflow:
+            callbackService:
+              annotations:
                 metallb.universe.tf/loadBalancerIPs: ${metallb_lb_ip}
-            workflow:
-                callbackService:
-                annotations:
-                    metallb.universe.tf/loadBalancerIPs: ${metallb_lb_ip}
+        ingress:
+          create:
+            domain: ${fz_domain}
+            proxy:
+              annotations:
+                metallb.universe.tf/loadBalancerIPs: ${metallb_lb_ip}
+        keycloak:
+          create:
+            realmId: ${keycloak_uuid}
             ingress:
-            create:
-                domain: ${fz_domain}
-                proxy:
-                annotations:
-                    metallb.universe.tf/loadBalancerIPs: ${metallb_lb_ip}
-            keycloak:
-            create:
-                realmId: ${keycloak_uuid}
-                ingress:
-                hostname: auth.${keycloak_domain}
-            database:
-            create:
-                credentials:
-                password: password
-        metallb_pool:
-        spec:
-            addresses:
-            - ${metallb_lb_ip}/32
-        keycloak_env:
-        KC_DB_PASSWORD: password
-        KEYCLOAK_ADMIN_PASSWORD: password
-        KC_HOSTNAME_URL: https://auth.${keycloak_domain}
-        KC_HOSTNAME_ADMIN_URL: https://authadmin.${keycloak_domain}
-        keycloak_certbot_email: noreply@ciq.co
-        keycloak_certbot_domain: "auth.${keycloak_domain},authadmin.${keycloak_domain}"
+              hostname: auth.${keycloak_domain}
+        database:
+          create:
+            credentials:
+              password: password
+    metallb_pool:
+      spec:
+        addresses:
+        - ${metallb_lb_ip}/32
+    keycloak_env:
+      KC_DB_PASSWORD: password
+      KEYCLOAK_ADMIN_PASSWORD: password
+      KC_HOSTNAME_URL: https://auth.${keycloak_domain}
+      KC_HOSTNAME_ADMIN_URL: https://authadmin.${keycloak_domain}
+    keycloak_certbot_email: noreply@ciq.co
+    keycloak_certbot_domain: "auth.${keycloak_domain},authadmin.${keycloak_domain}"
 EOF
 
     cat << EOF >> hosts.yaml
-    children:
-        admin:
-        hosts:
+  children:
+    admin:
+      hosts:
 EOF
     echo "$TF_OUTPUT" | jq -r '.admin_nodes_public_ip.value | to_entries[] | "\(.key)=\(.value)"' | while IFS="=" read -r key value; do
     cat << EOF >> hosts.yaml
-            ${key##*_}:
-            ansible_host: $value
-            cluster_interface: "enp8s0"
+        ${key##*_}:
+          ansible_host: $value
+          cluster_interface: "enp8s0"
 EOF
     done
 
     cat << EOF >> hosts.yaml
-        controller:
-        hosts:
+    controller:
+      hosts:
 EOF
     echo "$TF_OUTPUT" | jq -r '.controller_node_public_ips.value | to_entries[] | "\(.key)=\(.value)"' | while IFS="=" read -r key value; do
     internal_ip=$(echo "$TF_OUTPUT" | jq -r ".controller_node_private_ips.value.private_IP_${key##*_}")
     cat << EOF >> hosts.yaml
-            ${key##*_}:
-            ansible_host: $value
-            cluster_interface: "enp8s0"
-            rke2_node_ip: $internal_ip
+        ${key##*_}:
+          ansible_host: $value
+          cluster_interface: "enp8s0"
+          rke2_node_ip: $internal_ip
 EOF
     done
     cat << EOF >> hosts.yaml
@@ -392,9 +392,9 @@ EOF
 EOF
     echo "$TF_OUTPUT" | jq -r '.compute_instances_public_ip.value | to_entries[] | "\(.key)=\(.value)"' | while IFS="=" read -r key value; do
     cat << EOF >> hosts.yaml
-            ${key##*_}:
-            ansible_host: $value
-            cluster_interface: "enp8s0"
+        ${key##*_}:
+          ansible_host: $value
+          cluster_interface: "enp8s0"
 EOF
     done
 
@@ -475,6 +475,7 @@ function wipe() {
     if [[ "$choice" =~ ^[Yy]$ ]]; then
         rm -f "vultr/terraform.tfvars"
         rm -f ".env.sh"
+        echo "Removed..." 
     else
         echo "Aborted."
         exit 1
